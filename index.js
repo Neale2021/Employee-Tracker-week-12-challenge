f875dc41c9e9e788a0e2dc5b204d2a6de3d24ae4
Neale Philippe <neale@adflexmarketing.com.au>
2022-05-23 13:10:44 +1000

    finished index.js

--- index.js
+++ index.js
@@ -126,3 +126,273 @@ veiwAllEmployees = () => {
       userPrompts();
     })
   };
+// function to add a department to DB
+addDepartment = () => {
+    inquirer.prompt([
+      {
+        type: 'input', 
+        name: 'adddepartment',
+        message: "What department do you want to add?",
+        validate: adddepartment => {
+          if (adddepartment) {
+              return true;
+          } else {
+              console.log('Please enter a department');
+              return false;
+          }
+        }
+      }
+    ])
+      .then(answer => {
+        const sql = `INSERT INTO department (department_name)
+                    VALUES (?)`;
+        connection.query(sql, answer.adddepartment, (err, result) => {
+          if (err) return err;
+          console.log('Added ' + answer.adddepartment + " to departments!"); 
+          console.table(result);
+  
+         
+          userPrompts();
+      });
+    });
+  };
+  
+  // function to add a role to DB 
+  addRole = () => {
+    inquirer.prompt([
+      {
+        type: 'input', 
+        name: 'role',
+        message: "What role do you want to add?",
+        validate: addRole => {
+          if (addRole) {
+              return true;
+          } else {
+              console.log('Please enter a role');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input', 
+        name: 'salary',
+        message: "What is the salary of this role?",
+        validate: addSalary => {
+          if ((addSalary)) {
+              return true;
+          } else {
+              console.log('Please enter a salary');
+              return false;
+          }
+        }
+      }
+    ])
+          .then(answer => {
+            const params = [answer.role, answer.salary];
+      
+            // grabbing departmentartment from department table
+            const roleSql = `SELECT department_name, id FROM department`; 
+      
+            connection.query(roleSql, (err, rows) => {
+              if (err) return err; 
+          
+              const department = rows.map(({ department_name, id }) => ({ name: department_name, value: id }));
+      
+              inquirer.prompt([
+              {
+                type: 'list', 
+                name: 'department',
+                message: "What department is this role in?",
+                choices: department
+              }
+              ])
+                .then(departmentChoice => {
+                  const department = departmentChoice.department;
+                  params.push(department);
+      
+                  const sql = `INSERT INTO role (title, salary, department_id)
+                              VALUES (?, ?, ?)`;
+      
+                  connection.query(sql, params, (err, result) => {
+                    if (err) return err;
+                    console.log('Added' + answer.role + " to roles!"); 
+      
+                    userPrompts();
+             });
+           });
+         });
+       });
+      };
+// function to add an employee to DB 
+addEmployee = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: "What is the employee's first name?",
+        validate: addFirst => {
+          if (addFirst) {
+              return true;
+          } else {
+              console.log('Please enter a first name');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: "What is the employee's last name?",
+        validate: addLast => {
+          if (addLast) {
+              return true;
+          } else {
+              console.log('Please enter a last name');
+              return false;
+          }
+        }
+      }
+    ])
+      .then(answers => {
+      const params = [answers.firstName, answers.lastName]
+  
+      // grabbingbing roles from roles DB
+      const roleSql = `SELECT role.id, role.title FROM role`;
+    
+      connection.query(roleSql, (err, data) => {
+        if (err) return err; 
+        
+        const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+  
+        inquirer.prompt([
+              {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role?",
+                choices: roles
+              }
+            ])
+              .then(roleChoice => {
+                const role = roleChoice.role;
+                params.push(role);
+  
+      // grabbingbing the department from department table
+      const debtSql = `SELECT department_name, id FROM department`; 
+      
+      connection.query(debtSql, (err, rows) => {
+        if (err) return err; 
+    
+        const department = rows.map(({ department_name, id }) => ({ name: department_name, value: id }));
+  
+        inquirer.prompt([
+        {
+          type: 'list', 
+          name: 'department',
+          message: "What department is this role in?",
+          choices: department
+        }
+        ])
+          .then(departmentChoice => {
+            const department = departmentChoice.department;
+            params.push(department);
+  
+                const managerSql = `SELECT * FROM employee`;
+  
+                connection.query(managerSql, (err, data) => {
+                  if (err) return err;
+  
+                  const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+  
+                  
+  
+                  inquirer.prompt([
+                    {
+                      type: 'list',
+                      name: 'manager',
+                      message: "Who is the employee's manager?",
+                      choices: managers
+                    }
+                  ])
+                    .then(managerChoice => {
+                      const manager = managerChoice.manager;
+                      params.push(manager);
+  
+                      const sql = `INSERT INTO employee (first_name, last_name, role_id, department_id, managerId)
+                      VALUES (?, ?, ?, ?, ?)`;
+  
+                      connection.query(sql, params, (err, result) => {
+                      if (err) return err;
+                      console.log('Added' + result + " to employees!"); 
+  
+                      veiwAllEmployees();
+                      userPrompts();
+                });
+              });
+            });
+          });
+       });
+    });
+  })})
+  };
+  // function to update an employee 
+  updateEmployeeRole = () => {
+    // get employees from employee table 
+    const employeeSql = `SELECT * FROM employee`;
+  
+    connection.query(employeeSql, (err, data) => {
+      if (err) return err; 
+  
+    const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+  
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'name',
+          message: "Which employee would you like to update?",
+          choices: employees
+        }
+      ])
+        .then(employeeChoice => {
+          const employee = employeeChoice.name;
+          const params = []; 
+          params.push(employee);
+  
+          const roleSql = `SELECT * FROM role`;
+  
+          connection.query(roleSql, (err, data) => {
+            if (err) return err; 
+  
+            const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+            
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'role',
+                  message: "What is the employee's new role?",
+                  choices: roles
+                }
+              ])
+                  .then(roleChoice => {
+                  const role = roleChoice.role;
+                  params.push(role); 
+                  
+                  let employee = params[0]
+                  params[0] = role
+                  params[1] = employee 
+                  
+  
+                  // console.log(params)
+  
+                  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  
+                  connection.query(sql, params, (err, result) => {
+                    if (err) throw err;
+                  console.log("Employee has been updated!");
+                
+                  veiwAllEmployees();
+            });
+          });
+        });
+      });
+    });
+  };  
